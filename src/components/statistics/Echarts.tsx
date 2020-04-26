95cf4738aaa0768e562569db11c678a04e2b1c49
huanghaowei@mwee.cn <huanghaowei@mwee.cn>
2020-04-26 12:59:35 +0800

    采用redux-saga

---
+++ src/components/statistics/Echarts.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const Echarts = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Echarts
--- src/redux/configureStore.ts
+++ src/redux/configureStore.ts
@@ -1,17 +1,31 @@
+/*
+ * @Author: your name
+ * @Date: 2020-04-22 13:06:19
+ * @LastEditTime: 2020-04-26 10:28:37
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings
+ * @FilePath: /react-project/src/redux/configureStore.ts
+ */
 import { createStore, compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 import rootReducer from './modules';
 
-const middleware = applyMiddleware(thunk, logger)
+const sagaMiddleward = createSagaMiddleware();
+
+const middleware = applyMiddleware(sagaMiddleward, logger);
+
 const store: any =
     process.env.NODE_ENV === 'production'
         ? createStore(rootReducer, middleware)
         : (window as any).__REDUX_DEVTOOLS_EXTENSION__
         ? createStore(
               rootReducer,
-              compose(middleware, (window as any).__REDUX_DEVTOOLS_EXTENSION__())
+              compose(
+                  middleware,
+                  (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
+              ),
           )
-        : createStore(rootReducer, middleware)
+        : createStore(rootReducer, middleware);
 
 export default store;
